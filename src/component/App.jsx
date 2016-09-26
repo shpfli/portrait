859da00deced287069df78d270e6da00a61b56d5
hubery <Hubery.Lee@outlook.com>
2016-09-26 18:11:56 +0800

    init header of homepage!

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
-import { DatePicker } from 'antd';
+import React, {Component} from 'react';
 import './App.less';
+import '../stylesheets/github-dark.css';
+import '../stylesheets/styles.css';
 
 class App extends Component {
   render() {
-    return <DatePicker />;
+    return <div className="wrapper">
+      <header class="without-description">
+        <h1>Welcome to the Hubery's Homepage
+        </h1>
+        <p></p>
+        <p class="view">
+          <a href="https://github.com/shpfli">View the author on GitHub
+            <small>shpfli</small>
+          </a>
+        </p>
+        <ul>
+          <li class="single">
+            <a href="https://github.com/shpfli/portrait">View On
+              <strong>GitHub</strong>
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
   }
 }
 
 export default App;
-
