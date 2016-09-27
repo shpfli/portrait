b77a821f121d4a9dae708e8f010da35cbec298aa
hubery <Hubery.Lee@outlook.com>
2016-09-27 18:28:12 +0800

    init

---
+++ src/component/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class HomePage extends Component{
+  constructor() {
+    super()
+  }
+
+  render(){
+    return <div className="container">
+      Welcome to my homepage!
+    </div>
+  }
+}
+
+export default HomePage;
--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,6 +1,7 @@
 import '../common/lib';
 import App from '../component/App';
+import HomePage from '../component/HomePage';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-ReactDOM.render(<App />, document.getElementById('react-content'));
+ReactDOM.render(<HomePage />, document.getElementById('react-content'));
