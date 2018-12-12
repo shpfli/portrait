0ad1dd6b803b517b924ae278ee3cfe1d2daf59c6
hubery <Hubery.Lee@outlook.com>
2018-12-13 01:31:54 +0800

    fix version problem to make work

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import './common/lib';
+import Homepage from './component/Homepage';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.render(<Homepage />, document.getElementById('react-content'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,56 @@
-module.exports = function(webpackConfig) {
-  webpackConfig.babel.plugins.push('antd');
+// module.exports = function(webpackConfig) {
+//   webpackConfig.babel.plugins.push('antd');
+//
+//   // Fix ie8 compatibility
+//   webpackConfig.module.loaders.unshift({
+//     test: /\.jsx?$/,
+//     loader: 'es3ify-loader',
+//   });
+//
+//   return webpackConfig;
+// };
+// const webpack = require('webpack');
+const path = require('path');
 
-  // Fix ie8 compatibility
-  webpackConfig.module.loaders.unshift({
-    test: /\.jsx?$/,
-    loader: 'es3ify-loader',
-  });
+const config = {
+  mode: 'development',
+  entry: './src/entry/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  },
+  devServer: {
+    // contentBase: './dist'
+  }
+}
 
-  return webpackConfig;
-};
+module.exports = config;
